38045b80306265bad13c64144fe5d36e6daff884
KasperiVaarala <n1vaka00@students.oamk.fi>
2022-02-21 19:42:17 +0200

    apihomma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+const URL = 'https://www.themealdb.com/api/json/v1';
+
+
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+   const category = '1/categories.php' 
+   const address = URL + '/' + category
+
+   axios.get(address)
+   .then((response) => {
+    
+     setItems(response.data.categories);
+     setIsLoaded(true);
+
+   }).catch(error =>{
+     setError(error);
+
+   });
+  }, [])
+
+  function close(){
+    setSelectedItem(null)
+  }
+
+  
+
+if (selectedItem !=null){
+  return 
+ 
+} else if(error) {
+    return <p>ey bruh that don't work</p>
+  }else if (!isLoaded) {
+  return <p>Food coming, hold up</p>
+  } else {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h1>Food categories</h1>
+      <div>
+        {
+          items.map(item => (
+            <div>
+            
+              <h3>{item.strCategory}</h3>
+              <img src={item.strCategoryThumb} />
+            <p>{item.strCategoryDescription}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
+
   );
+}
 }
 
 export default App;
